9ba9f5bb77ed68dcfea6ccf0080c4f21e13d9949
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-01-01 23:12:10 -0300

    Cadastro de Projetos Realizado

--- index.js
+++ index.js
@@ -2,6 +2,31 @@ const express = require('express');
 const app = express();
 const projetos = require('./database/database')
 
+app.use(express.json())
+
+app.post('/projetos', (req, res) => {
+    const novoProjeto = {
+        nome: req.body.nome,
+        descricao: req.body.descricao,
+        dataCriacao: req.body.dataCriacao
+    }
+
+    if (typeof req.body.nome !== "string") {
+        return res.status(406).send('Informação Incorreta Cadastrada')
+    }
+    
+    if (typeof req.body.descricao !== "string") {
+        return res.status(406).send('Informação Incorreta Cadastrada')
+    }
+
+    if (typeof req.body.dataCriacao !== "string") {
+        return res.status(406).send('Informação Incorreta Cadastrada')
+    }
+
+    projetos.push(novoProjeto)
+    res.send('Projeto Cadastrado com Sucesso!')
+})
+
 app.listen(8081, () => {
     console.log('Servidor Rodando em http://localhost:8081')
 })
