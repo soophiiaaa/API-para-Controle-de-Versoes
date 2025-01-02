caaaf3fe822dc04e7cadb9f9a51dc72415de97f7
Sophia Lacerda <sbuarquemagalhaes@gmail.com>
2025-01-01 22:45:34 -0300

    Realizada Exportação de Simulação de Banco de Dados

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const app = express()
+const app = express();
+const projetos = require('./database/database')
 
 app.listen(8081, () => {
     console.log('Servidor Rodando em http://localhost:8081')
